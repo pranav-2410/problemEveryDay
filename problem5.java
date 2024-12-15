9b1e9823ff2d39df9f4e5b8524eab8bb210af1ac
Pranav P <pranav0024p@gmail.com>
2024-12-15 15:51:39 +0530

    LEETCODE:: 206 REVERSE LINKED LIST

--- problem5.java
+++ problem5.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class problem5 {
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/*LEETCODE:: 206
+REVERSE LINKED LIST
+ */
+public class problem5 {
+
+    public static void main(String[] args) {
+
+        problem5 problem = new problem5();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2, node1);
+        ListNode node3 = new ListNode(3, node2);
+        ListNode node4 = new ListNode(4, node3);
+        System.out.println("BEFORE:: ");
+        problem.printLinkedList(node4);
+        ListNode ans = problem.reverseList(node4);
+        System.out.println("AFTER:: ");
+        problem.printLinkedList(ans);
+    }
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode cur = head;
+
+        ListNode next = null;
+        while(cur!= null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    public void printLinkedList (ListNode head){
+        ListNode temp = head;
+        while (temp!=null) {
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+    }
 }
+
